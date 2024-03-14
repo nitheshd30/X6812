69afa1eeb7f860cd21da7f9a08eacb801b81da02
NITHESH <112474206+nitheshd30@users.noreply.github.com>
2024-03-14 21:16:52 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/shiva
+DEVICE_PATH := device/infinix/X6812
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
